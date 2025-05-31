6cf0cc6a015bd75e686354815cc0a1caf008a5d0
Ramon Lima <lima.ramon13@gmail.com>
2025-05-31 14:00:33 -0300

    Initial commit

---
+++ src/main/java/com/dev13/scorezada_api/ScorezadaApiApplication.java
@@ -0,0 +1,13 @@
+package com.dev13.scorezada_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScorezadaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScorezadaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev13/scorezada_api/ScorezadaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev13.scorezada_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScorezadaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
